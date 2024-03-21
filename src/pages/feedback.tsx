f2458ed15570f166b89245958f39b443332ae1a4
Saksham <saksham.gusain12@gmail.com>
2024-03-21 09:28:21 +0530

    added feedback molecule

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import FeedbackPage from '../components/MorePage/FeedbackPage';
+import FeedbackPage from '../components/MorePage/FeedbackPage/index';
 import { useLocalization } from '../hooks';
 
 
