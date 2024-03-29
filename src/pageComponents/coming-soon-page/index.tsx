3c56342f3e371b869140c7dae43c8ab75abc230b
riya-2206 <riya@esmagico.in>
2024-03-29 11:39:57 +0530

    added localization for tranlation json

--- src/pageComponents/coming-soon-page/index.tsx
+++ src/pageComponents/coming-soon-page/index.tsx
@@ -5,14 +5,13 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Hourglass from './hourglass';
 import { useColorPalates } from '../../providers/theme-provider/hooks';
-import { useConfig } from '../../hooks/useConfig';
+import { useLocalization } from '../../hooks';
 
 const ComingSoonPage: React.FC = () => {
+  const t = useLocalization();
   const theme = useColorPalates();
-  const config = useConfig('component', 'comingSoon');
   const handleBack = useCallback(()=>{
     window?.history?.back()
-    // console.log(component.backText ?? "Back Button")
   },[])
 
   return (
@@ -21,10 +20,10 @@ const ComingSoonPage: React.FC = () => {
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
         <Box my={15} className={styles.container}>
-          <Box mt={5}><Typography variant='h4' sx={{color: theme?.primary?.main, fontWeight: "700"}}>{config?.title ?? "Coming Soon"}</Typography></Box>
+          <Box mt={5}><Typography variant='h4' sx={{color: theme?.primary?.main, fontWeight: "700"}}>{t("message.coming_soon")}</Typography></Box>
           <Box><Hourglass fillColor={theme?.primary?.main}/></Box>
-          <Box><Typography variant='body1' sx={{fontWeight:"600"}}>{config?.description?? "Coming Soon Description"}</Typography></Box>
-          <Box my={5}><Button variant='contained' className={styles.backButton} size='large' style={{backgroundColor: theme?.primary?.main}} onClick={handleBack}>{config?.backText ?? "Back Button"}</Button></Box>
+          <Box><Typography variant='body1' sx={{fontWeight:"600"}}>{t("message.coming_soon_description")}</Typography></Box>
+          <Box my={5}><Button variant='contained' className={styles.backButton} size='large' style={{backgroundColor: theme?.primary?.main}} onClick={handleBack}>{t("label.back")}</Button></Box>
         </Box>
     </>
   );
