fd15e516bb69f6c881a3ce0188349b9e609b66af
Birat Datta <88641794+biratdatta@users.noreply.github.com>
2024-07-08 14:48:32 +0530

    style: new UI changes (#256)

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import HomeIcon from '@mui/icons-material/Home';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import MicNoneIcon from '@mui/icons-material/MicNone';
 import Button from '@mui/material/Button';
@@ -10,6 +11,7 @@ import { useLocalization } from '../../hooks';
 const Menu = () => {
   const router = useRouter();
   const t = useLocalization();
+  const isHome = router.pathname === '/';
 
   const handleHomeClick = () => {
     router.push('/');
@@ -24,15 +26,19 @@ const Menu = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.buttonWrapper}>
-        <Button onClick={handleHomeClick} className={`  ${styles.greenButton}`}>
-          <HomeIcon fontSize="large" />
+        <Button onClick={handleHomeClick} className={`${styles.greenButton}`}>
+          {isHome ? (
+            <HomeOutlinedIcon fontSize="large" />
+          ) : (
+            <HomeIcon fontSize="large" />
+          )}
         </Button>
         <p className={styles.buttonText}>{t('label.menu_home')}</p>
       </div>
       <div className={styles.middleButton}>
         <Button
           onClick={handleTouchToSpeakClick}
-          className={`  ${styles.touchToSpeakButton}`}
+          className={`${styles.touchToSpeakButton}`}
           startIcon={<MicNoneIcon fontSize="inherit" />}
         >
           {t('label.menu_tap_text')}
@@ -41,7 +47,7 @@ const Menu = () => {
       <div className={styles.buttonWrapper}>
         <Button
           onClick={handleNotificationClick}
-          className={`  ${styles.greenButton}`}
+          className={`${styles.greenButton}`}
         >
           <NotificationsNoneIcon fontSize="large" />
         </Button>
