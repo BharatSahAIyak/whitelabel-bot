415c59405eaf9edd07ec9e68e8830a805673aa19
prtkjakhar <prtkjakhar@gmail.com>
2024-07-11 14:23:36 +0530

    fix: bug fixes
     Please enter the commit message for your changes. Lines starting

--- src/providers/context-provider.tsx
+++ src/providers/context-provider.tsx
@@ -406,14 +406,7 @@ const ContextProvider: FC<{
   const sendMessage = useCallback(
     async (textToSend: string, textToShow: string, media: any, isVisibile = true) => {
       if (!textToShow) textToShow = textToSend;
-      if (!sessionStorage.getItem('conversationId')) {
-        const cId = uuidv4();
-        console.log('convId', cId);
-        setConversationId(() => {
-          sessionStorage.setItem('conversationId', cId);
-          return cId;
-        });
-      } else sessionStorage.setItem('conversationId', conversationId || '');
+
       // if (!localStorage.getItem('userID')) {
       //   removeCookie('access_token', { path: '/' });
       //   location?.reload();
@@ -426,6 +419,7 @@ const ContextProvider: FC<{
       console.log('s2tMsgId:', s2tMsgId);
       const messageId = s2tMsgId ? s2tMsgId : uuidv4();
       console.log('s2t messageId:', messageId);
+      const cId = uuidv4();
       newSocket.sendMessage({
         payload: {
           app: process.env.NEXT_PUBLIC_BOT_ID || '',
@@ -448,10 +442,18 @@ const ContextProvider: FC<{
           },
           messageId: {
             Id: messageId,
-            channelMessageId: conversationId,
+            channelMessageId: sessionStorage.getItem('conversationId') || cId,
           },
         } as Partial<XMessage>,
       });
+
+      if (!sessionStorage.getItem('conversationId')) {
+        console.log('convId', cId);
+        setConversationId(() => {
+          sessionStorage.setItem('conversationId', cId);
+          return cId;
+        });
+      } else sessionStorage.setItem('conversationId', conversationId || '');
 
       setStartTime(Date.now());
       if (isVisibile)
@@ -524,26 +526,41 @@ const ContextProvider: FC<{
     console.log('in normalized', chats);
     const conversationId = sessionStorage.getItem('conversationId');
     const history = chats
-      .filter((item: any) => conversationId === 'null' || item.conversationId === conversationId)
-      .flatMap((item: any) =>
-        [
-          item.query?.length && {
-            text: item.query,
-            position: 'right',
-            repliedTimestamp: item.createdAt,
-            messageId: uuidv4(),
-          },
-          {
-            text: item.response,
-            position: 'left',
-            sentTimestamp: item.createdAt,
-            reaction: item.reaction,
-            msgId: item.id,
-            messageId: item.id,
-            audio_url: item.audioURL,
-            isEnd: true,
-          },
-        ].filter(Boolean)
+      .filter(
+        (item: any) =>
+          (conversationId === 'null' || item?.channelMessageId === conversationId) &&
+          (item?.to !== 'admin' || !item.payload?.metaData?.hideMessage)
+      )
+      .map((item: any) => ({
+        text: (item?.to === 'admin'
+          ? item?.payload?.metaData?.originalText ?? item?.payload?.text
+          : item?.payload?.text
+        )
+          ?.replace(/<end\/>/g, '')
+          ?.replace(/^Guided:/, ''),
+        position: item.to === 'admin' ? 'right' : 'left',
+        timestamp: item.timestamp,
+        reaction:
+          item?.feedback?.type === 'FEEDBACK_POSITIVE'
+            ? 1
+            : item?.feedback?.type === 'FEEDBACK_NEGATIVE'
+              ? -1
+              : 0,
+        msgId: item.messageId,
+        messageId: item.messageId,
+        replyId: item.replyId,
+        audio_url: item?.audioURL,
+        isEnd: true,
+        optionClicked: true,
+        // choices: item?.payload?.buttonChoices,
+        isGuided: item?.metaData?.isGuided,
+        card: item?.payload?.card,
+        choices: [],
+        conversationId: item?.channelMessageId,
+      }))
+      .sort(
+        //@ts-ignore
+        (a, b) => new Date(a.timestamp) - new Date(b.timestamp)
       );
 
     console.log('historyyy', history);
@@ -571,12 +588,12 @@ const ContextProvider: FC<{
           console.log('log:', loading);
           try {
             const chatHistory = await axios.get(
-              `${
-                process.env.NEXT_PUBLIC_BFF_API_URL
-              }/user/chathistory/${sessionStorage.getItem('conversationId')}`,
+              `${process.env.NEXT_PUBLIC_BFF_API_URL}/history?userId=${localStorage.getItem(
+                'userID'
+              )}&conversationId=${sessionStorage.getItem('conversationId')}`,
               {
                 headers: {
-                  authorization: `Bearer ${localStorage.getItem('auth')}`,
+                  botId: process.env.NEXT_PUBLIC_BOT_ID || '',
                 },
               }
             );
