136d299ec6dea7759cbdd897860e5e42f29c32f0
prtkjakhar <prtkjakhar@gmail.com>
2024-07-20 00:53:34 +0530

    chore: feature flags

---
+++ src/pages/api/flags.js
@@ -0,0 +1,32 @@
+export default async function handler(req, res) {
+  const { userType } = req.query;
+
+  if (!userType) {
+    return res.status(400).json({ error: 'User Type is required' });
+  }
+
+  try {
+    const flags = getFlags(userType);
+    return res.status(200).json({ flags });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Failed to fetch feature flags' });
+  }
+}
+
+const getFlags = (userType) => {
+  switch (userType) {
+    case 'VAW':
+      return {};
+    case 'farmer':
+      return {
+        component: {
+          menu: {
+            showMicButton: false,
+          },
+        },
+      };
+    default:
+      return {};
+  }
+};
