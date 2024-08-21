5478e085d8afa19cd77d38e2bd52d80a0317d9d6
prtkjakhar <prtkjakhar@gmail.com>
2024-08-21 15:47:15 +0530

    fix: notification duplicates

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,87 +1,36 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-firebase.initializeApp({
-  apiKey: true,
-  authDomain: true,
-  projectId: true,
-  storageBucket: true,
-  messagingSenderId: true,
-  appId: true,
-  measurementId: true,
+// Set Firebase configuration, once available
+self.addEventListener('fetch', () => {
+  const urlParams = new URLSearchParams(location.search);
+  self.firebaseConfig = Object.fromEntries(urlParams);
 });
 
+// "Default" Firebase configuration (prevents errors)
+const defaultConfig = {
+  apiKey: true,
+  projectId: true,
+  messagingSenderId: true,
+  appId: true,
+};
+
+// Initialize Firebase app
+firebase.initializeApp(self.firebaseConfig || defaultConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('Received background message:', JSON.stringify(payload));
   const { title, body, image } = payload.notification;
 
-  // Customize notification here
-  const sideLogo = payload.data.sideLogo;
-
-  const notificationOptions = {
-    body,
-    icon: sideLogo,
-    image: image,
-    tag: 'notification',
-    renotify: true,
-    data: { ...payload.data },
-  };
-  console.log('Notification Content:', JSON.stringify(notificationOptions));
-
-  self.registration.showNotification(title, notificationOptions);
-});
-
-// Handle notification clicks
-self.addEventListener('notificationclick', function (event) {
-  console.log('[firebase-messaging-sw.js] Notification click received.');
-
-  event.notification.close();
   const notificationData = {
-    title: event.notification.title,
-    body: event.notification.body,
-    image: event.notification.image,
-    url: event.notification.data?.url,
-    buttonText: event.notification.data?.buttonText,
-    buttonUrl: event.notification.data?.buttonUrl,
-    timestamp: new Date().getTime(),
+    title,
+    body,
+    icon: payload.data.sideLogo,
+    image: image,
+    ...payload.data,
   };
-  console.log('[firebase-messaging-sw.js] Notification click received 1 .', notificationData);
+  console.log('Notification Content:', JSON.stringify(notificationData));
 
-  event.waitUntil(
-    (async () => {
-      try {
-        // Store notification data in IndexedDB
-        const dbPromise = self.indexedDB.open('notificationDB', 1);
-
-        dbPromise.onupgradeneeded = (event) => {
-          const db = event.target.result;
-          db.createObjectStore('notifications', { keyPath: 'timestamp' });
-        };
-
-        dbPromise.onsuccess = async (event) => {
-          const db = event.target.result;
-          const transaction = db.transaction('notifications', 'readwrite');
-          const store = transaction.objectStore('notifications');
-          await store.put(notificationData);
-          console.log('Notification data stored:', notificationData);
-        };
-
-        const clientList = await clients.matchAll({ type: 'window', includeUncontrolled: true });
-        if (notificationData.url) {
-          for (const client of clientList) {
-            if (client.url === notificationData.url && 'focus' in client) {
-              return client.focus();
-            }
-          }
-          if (clients.openWindow) {
-            return clients.openWindow(notificationData.url);
-          }
-        }
-      } catch (error) {
-        console.error('Error handling notification click:', error);
-      }
-    })()
-  );
+  // return self.registration.showNotification(title, notificationData);
 });
