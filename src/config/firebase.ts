dc2c7bd1f9f944e9df5a8d47b72def49f09ef2cc
prtkjakhar <prtkjakhar@gmail.com>
2024-08-21 17:51:21 +0530

    fix: firebase config bug

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: String(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
+  authDomain: String(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
+  projectId: String(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
+  storageBucket: String(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
+  messagingSenderId: String(process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+  appId: String(process.env.NEXT_PUBLIC_FIREBASE_APP_ID),
+  measurementId: String(process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID),
 };
 
 let app;
@@ -17,7 +17,7 @@ let messaging: any;
 export const initializeFirebase = () => {
   if (typeof window !== 'undefined' && !getApps().length) {
     app = initializeApp(firebaseConfig);
-    messaging = getMessaging(app);
+    messaging = typeof window !== 'undefined' ? getMessaging(app) : null;
   }
 };
 
