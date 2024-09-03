4a8b43d75ee4b1f713aa656c0eec1b480279d80e
prtkjakhar <prtkjakhar@gmail.com>
2024-09-03 16:39:46 +0530

    fix: otp length parsed int

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   grep -rlE '12\\.1\\.6|17\\.0\\.2' .next/static/chunks | xargs sed -i '' 's/12\\.1\\.6/xx.xx.xx/g; s/17\\.0\\.2/xx.xx.xx/g'
 else
-  grep -rlE '12\\.1\\.6|17\\.0\\.2' /app/.next/static/chunks | xargs sed -i 's/12\\.1\\.6/xx.xx.xx/g; s/17\\.0\\.2/xx.xx.xx/g'
+  grep -rlE '12\\.1\\.6|17\\.0\\.2' .next/static/chunks | xargs sed -i 's/12\\.1\\.6/xx.xx.xx/g; s/17\\.0\\.2/xx.xx.xx/g'
 fi
